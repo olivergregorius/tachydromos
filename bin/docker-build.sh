a4fd0cd38a78f1cf4a352b41718ece6ea50f99c3
Oliver Gregorius <oliver@gregorius.dev>
2023-10-01 23:00:39 +0200

    Implemented connecting to an outbound LDAP server and PostgreSQL instance

---
+++ bin/docker-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <Docker project name>"
+  exit 1
+fi
+
+script_dir=$(dirname "$0")
+project_name=$1
+project_path="${script_dir}/../dockerfiles/${project_name}"
+
+# Determining Docker project parameters
+repository=$(yq -r .repository "${project_path}/properties.yml")
+tag=$(yq -r .tag "${project_path}/properties.yml")
+version=$(yq -r .version "${project_path}/properties.yml")
+arch=$(yq -r .arch "${project_path}/properties.yml")
+
+# Build Docker image
+echo "Building Docker image ${repository}:${tag}"
+docker buildx build \
+  --file "${project_path}/Dockerfile" \
+  --build-arg APPVERSION="$version" \
+  --platform "$arch" \
+  --tag "${repository}:${tag}" \
+  --push \
+  "$project_path"
